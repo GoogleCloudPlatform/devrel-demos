9476487a49fd1624d4d2dd1a5e56dfb5a446bd69
Luke Schlangen <lukeschlangen@gmail.com>
2023-08-15 17:18:01 -0500

    prepare for release (#90)
    * prepare for release
    
    * fix: fix git ignore

--- app-dev/cloud-quiz/app/(authenticated-pages)/layout.tsx
+++ app-dev/cloud-quiz/app/(authenticated-pages)/layout.tsx
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 "use client"
 
 import '@/app/globals.css';
@@ -35,7 +51,7 @@ export default function RootLayout({
                   priority
                 />
               </div>
-              <h1 className='text-4xl pt-10'>Cloud Quiz</h1>
+              <h1 className='text-4xl pt-10'>Party Game</h1>
               <h2 className='pb-10'>Test your knowledge of Google Cloud.</h2>
               <BigSignInButton />
             </center>
