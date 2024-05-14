e09828340221d52111a7042bbf3cfbd41ebf673e
Brad Miro <bmiro@google.com>
2024-05-14 16:38:50 -0400

    Update run.sh

--- data-analytics/data-dash/pi/run.sh
+++ data-analytics/data-dash/pi/run.sh
@@ -5,4 +5,4 @@ export PROJECT_ID=${1}
 cd /home/google/devrel-demos/data-analytics/data-dash/pi
 git pull https://github.com/GoogleCloudPlatform/devrel-demos.git
 . sensors/bin/activate 
-python rfid.py >> /home/google/xyz.txt 2>&1 &
+python run.py >> /home/google/xyz.txt 2>&1 &
