10b43e4fe4c28ee6eb03daf8c57100fd1b44a053
Emanuel Burgess <emanuelburgess@google.com>
2024-08-03 13:22:20 -0400

    gradio additions

---
+++ ai-ml/gemini-chatbot-app/gradio/gradio_app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Copyright 2024 Google, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import openai
+import gradio as gr
+import llm
+
+client = llm.get_chat_client(llm.PROJECT_ID, llm.LOCATION)
+
+def predict(message, history):
+    history_openai_format = []
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant", "content":assistant})
+    history_openai_format.append({"role": "user", "content": message})
+  
+    response = client.chat.completions.create(model=llm.MODEL_NAME,
+    messages= history_openai_format,
+    temperature = 1.0,
+    stream=True)
+
+    partial_message = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+              partial_message = partial_message + chunk.choices[0].delta.content
+              yield partial_message
+
+gr.ChatInterface(predict).launch(share=True)
