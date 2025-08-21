8908eabd2959b20cff5fa560b878d32d6ae5b88f
askmeegs <megan037@gmail.com>
2025-08-21 15:35:50 -0400

    replace google books with wikipedia

--- ai-ml/book-recommendations-agent-eval/README.md
+++ ai-ml/book-recommendations-agent-eval/README.md
@@ -3,15 +3,3 @@
 
 ### Prerequisites 
 
-- A Google Cloud project (for API credentials)
-- A Google Books API Key - navigate [here](https://console.cloud.google.com/apis/credentials). Click "Create Credentials" 
-
-
-Enable API 
-
-https://console.developers.google.com/apis/api/books.googleapis.com/ 
-
-
-```bash
-export GOOGLE_BOOKS_API_KEY="your-api-key"
-```
--- ai-ml/book-recommendations-agent-eval/book-recommendations/agent.py
+++ ai-ml/book-recommendations-agent-eval/book-recommendations/agent.py
@@ -1,22 +1,19 @@
 from google.adk.agents import Agent
 from google.adk.tools.langchain_tool import LangchainTool
 from google.adk.tools import google_search
-from langchain_community.tools.google_books import GoogleBooksQueryRun
-from langchain_community.utilities.google_books import GoogleBooksAPIWrapper
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+
 from google.adk.tools.agent_tool import AgentTool
 
 import os
 
-# Init Google Books API connection via LangChain tool + ADK integration
-google_books_api_key = os.getenv("GOOGLE_BOOKS_API_KEY")
-if not google_books_api_key:
-    raise ValueError("GOOGLE_BOOKS_API_KEY environment variable is not set")
-google_books_tool = GoogleBooksQueryRun(api_wrapper=GoogleBooksAPIWrapper())
-google_books_langchain_tool = LangchainTool(google_books_tool)
+langchain_wikipedia_tool = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+adk_wikipedia_tool = LangchainTool(tool=langchain_wikipedia_tool)
 
 # Built-in Google Search tool
 search_agent = Agent(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-pro",
     name="search_agent",
     instruction="""
     You are a Google Search Agent. You are part of a larger workflow designed to recommend novels to users. Your job is to take the title/author of a book, and gather some more info about it, to help the user decide if they want to read it. For instance, find its Goodreads star rating, or testimonials from forums like Reddit as to whether the book is worth reading.
@@ -33,17 +30,20 @@ SUGGESTED WORKFLOW:
 - GATHER USER REQUIREMENTS - ask the user what genre or type of novel they want to read. (eg. fantasy, historical fiction, literary fiction?). If the user doesn't provide detailed requirements at first, ask them for a bit more detail (setting? mood? or a book they read recently that they liked?)
 - FORMULATE A GOOGLE SEARCH QUERY - distill the user's requirements into a short (8 words or less) search query for Google Search. Always make sure "novel" is in the search query.  
 - SEARCH GOOGLE FOR TOP RESULTS 
-- For the top search result, search GOOGLE BOOKS to get the published synopsis. 
+- For the top search result, search WIKIPEDIA (adk_wikipedia_tool) to get the publication date and synopsis. 
 - Do another GOOGLE SEARCH to get other key info about the book, like blurbs or book reviews / testimonials.
 - Send back the book title, author, synopsis, and any other key info you found. 
 - Ask the user if it's suitable - if they say no, or they've already read it, find another book from your initial search results, and gather info about it. 
 
-Special instructions - be brief in your responses and questions, and use lots of emojis for fun! 
+Special instructions 
+- Make sure your initial Google Search looks specifically for book titles that you can learn more about later.
+- Always use the ADK Wikipedia Tool to get the book's synopsis and publication date. Make sure both are in your final output BEFORE responding. 
+- be brief in your responses and questions, and use lots of emojis for fun! 
 """
 
 root_agent = Agent(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-pro",
     name="book_recommendations",
     instruction=agent_instruction,
-    tools=[google_books_langchain_tool, search_tool],
+    tools=[adk_wikipedia_tool, search_tool],
 )
