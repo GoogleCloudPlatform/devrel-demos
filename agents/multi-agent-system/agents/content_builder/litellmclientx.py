2f25d9f0fd549e4f4a7502d1f7a9a915a9d66700
minherz <leoy@google.com>
2026-04-17 12:24:19 -0700

    fix: ensure that each call to the gemma model has up to date token

---
+++ agents/multi-agent-system/agents/content_builder/litellmclientx.py
@@ -0,0 +1,55 @@
+import os
+from typing import Any, Optional, Union
+from google.adk.models.lite_llm import LiteLLMClient
+from litellm import CustomStreamWrapper, ModelResponse
+
+
+def get_auth_token() -> Optional[str]:
+    """Obtains a Google ID token for the given audience (Cloud Run service URL)."""
+    audience = os.getenv("OLLAMA_API_BASE", "")
+    try:
+        creds, _ = google.auth.default()
+        print("DEBUG: acquiring credentials")
+        # if user credentials are used
+        if hasattr(creds, "id_token") and creds.id_token:
+            print("DEBUG: ID token in credentials")
+            return creds.id_token
+        print("DEBUG: trying to refresh")
+        auth_req = google.auth.transport.requests.Request()
+        creds.refresh(auth_req)
+        if hasattr(creds, "id_token") and creds.id_token:
+            print("DEBUG: ID token acquired after refresh")
+            return creds.id_token
+        # if service account credentials are used
+        fetched_id_token = id_token.fetch_id_token(auth_req, audience)
+        print(f"DEBUG: ID token fetched with audience '{audience}'")
+        return fetched_id_token
+    except Exception as e:
+        print(f"Error obtaining ID token for {audience}: {e}")
+        return None
+
+
+class AuthLiteLLMClient(LiteLLMClient):
+    """
+    Overrides the LiteLLMClient to inject a custom httpx.AsyncClient.
+    """
+
+    def __init__(self, **data: Any) -> None:
+        super().__init__(**data)
+
+    async def acompletion(
+        self, model, messages, tools, **kwargs
+    ) -> Union[ModelResponse, CustomStreamWrapper]:
+        """
+        Updating headers to inject bearer token.
+        """
+
+        token: str = get_auth_token()
+        if "extra_headers" not in kwargs or kwargs["extra_headers"] is None:
+            kwargs["extra_headers"] = {}
+        kwargs["extra_headers"]["Authorization"] = f"Bearer {token}"
+
+        # Ensure we call the parent class acompletion to preserve ADK internal logic
+        return await super().acompletion(
+            model=model, messages=messages, tools=tools, **kwargs
+        )
