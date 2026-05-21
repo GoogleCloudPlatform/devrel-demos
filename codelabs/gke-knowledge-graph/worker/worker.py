e2b71d1438e5b9a4b3f3d966ecd25debb07377d1
unicorn <subatin@google.com>
2026-05-21 18:42:00 +0000

    Fix worker logging and Pub/Sub ack deadline performance bottleneck in GKE codelab

--- codelabs/gke-knowledge-graph/worker/worker.py
+++ codelabs/gke-knowledge-graph/worker/worker.py
@@ -2,11 +2,42 @@
 import os
 import sys
 import time
+import socket
+import logging
 from google.cloud import pubsub_v1
 from google.api_core.exceptions import DeadlineExceeded
 from process_file import process_single_file
 
+class LoggerWriter:
+    def __init__(self, writer):
+        self.writer = writer
+        self.buf = ""
+
+    def write(self, message):
+        self.buf += message
+        if "\n" in self.buf:
+            parts = self.buf.split("\n")
+            for part in parts[:-1]:
+                if part.strip():
+                    self.writer(part)
+            self.buf = parts[-1]
+
+    def flush(self):
+        if self.buf.strip():
+            self.writer(self.buf)
+        self.buf = ""
+
+
 def main():
+    hostname = socket.gethostname()
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f'[{hostname}] %(asctime)s %(levelname)s %(message)s',
+        stream=sys.stdout
+    )
+    sys.stdout = LoggerWriter(logging.info)
+    sys.stderr = LoggerWriter(logging.error)
+
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
     subscription_name = os.environ.get("PUBSUB_SUBSCRIPTION", "petverse-sub")
 
